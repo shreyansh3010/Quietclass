2d702eaaa69a452a3d9e9249a5b6e7944d92ea4c
shreyansh <shreyansh.2015@vit.ac.in>
2017-07-27 13:13:07 +0530

    add the gps listener to the app

--- app/src/main/java/com/shreyanshvit/quietclass/MainActivity.java
+++ app/src/main/java/com/shreyanshvit/quietclass/MainActivity.java
@@ -1,17 +1,32 @@
 package com.shreyanshvit.quietclass;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Vibrator;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mStart, mStop;
     Vibrator vibrator;
+
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 
             }
         });
@@ -39,5 +54,70 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        textView = (TextView) findViewById(R.id.textView);
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n " + location.getLongitude() + " " + location.getLatitude());
+                if(location.getLongitude()>=79.158797 && location.getLongitude()<= 79.1587890 && location.getLatitude()<=12.973837 && location.getLatitude()>=12.9734119) {
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                }
+                else{
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 2000, 0, listener);
+        // this code won'textView execute IF permissions are not allowed, because in the line above there is return statement.
+
     }
 }
